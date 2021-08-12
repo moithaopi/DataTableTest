2785b5ed9aba9a9bd88ce1bb4306c76695f45836
Moithaopi Monwela <moithaopimonwela@gmail.com>
2021-08-12 21:42:56 +0200

    update

--- myscript.js
+++ myscript.js
@@ -1,4 +1,34 @@
+var minDate, maxDate;
+ 
+// Custom filtering function which will search data in column four between two values
+$.fn.dataTable.ext.search.push(
+    function( settings, data, dataIndex ) {
+        var min = minDate.val();
+        var max = maxDate.val();
+        var date = new Date( data[4] );
+ 
+        if (
+            ( min === null && max === null ) ||
+            ( min === null && date <= max ) ||
+            ( min <= date   && max === null ) ||
+            ( min <= date   && date <= max )
+        ) {
+            return true;
+        }
+        return false;
+    }
+);
+
 $(document).ready(function(){
+
+    // Create date inputs
+    minDate = new DateTime($('#from'), {
+        format: 'MMMM Do YYYY'
+    });
+    maxDate = new DateTime($('#to'), {
+        format: 'MMMM Do YYYY'
+    });
+
     $("#my_table").DataTable(
         {
             "ajax":{
@@ -16,4 +46,11 @@ $(document).ready(function(){
             },
         }
     );
+
+    // Refilter the table
+    $('#from, #to').on('change', function () {
+        table.draw();
+    });
+
 });
+
